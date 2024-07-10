92166f4c1198c0ea18ab6734ef5396ea41222f03
Ubuntu <ubuntu@ip-10-161-51-173.ec2.internal>
2024-07-10 01:22:09 +0000

    constant lr unhack local data path.

--- lerobot/common/datasets/factory.py
+++ lerobot/common/datasets/factory.py
@@ -91,13 +91,14 @@ def make_dataset(cfg, split: str = "train") -> LeRobotDataset | MultiLeRobotData
         )
 
     if isinstance(cfg.dataset_repo_id, str):
+        root = cfg.get("dataset_local_root_dir", None)
         dataset = LeRobotDataset(
             cfg.dataset_repo_id,
             split=split,
             delta_timestamps=cfg.training.get("delta_timestamps"),
             image_transforms=image_transforms,
             video_backend=cfg.video_backend,
-            root="/home/ubuntu/efs/sfeng/lerobot/",
+            root=root,
         )
     else:
         dataset = MultiLeRobotDataset(
